54e0598f9cacc2bf8aa769d09eb7322720c1bd43
NOVOTEC-NAKA <00001101752452sp@aluno.educacao.sp.gov.br>
2023-03-17 11:03:46 -0300

    17.3.23

--- inserirEmpresas.sql
+++ inserirEmpresas.sql
@@ -1,1 +1,10 @@
+ALTER TABLE empresas MODIFY cnpj VARCHAR(14)
+
+INSERT INTO empresas 
+    (nome, cnpj)
+VALUES
+    ("Bradesco", "9813729323"),
+    ("Vale", "3234123444"),
+    ("Cielo", "23232434233")
+
 SELECT * FROM empresas
