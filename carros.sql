ca5cd6550b42c82d6780b109a1040dd8a564208b
NOVOTEC-NAKA <00001101752452sp@aluno.educacao.sp.gov.br>
2023-03-21 10:22:25 -0300

    21.3.23

--- carros.sql
+++ carros.sql
@@ -31,9 +31,10 @@ CREATE TABLE IF NOT EXISTS carros (
 );
 
 CREATE TABLE IF NOT EXISTS vendas (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    
     nome_do_funcionario VARCHAR(255) NOT NULL,
     
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     comissao INT UNSIGNED NOT NULL,
     valor_da_venda DECIMAL(6,2) NOT NULL,
     
@@ -52,6 +53,9 @@ SELECT * FROM funcionarios
 SELECT * FROM carros
 SELECT * FROM vendas
 
+DROP TABLE vendas
+DROP TABLE `funcionarios`
+DELETE FROM vendas WHERE id =1;
 
 INSERT INTO clientes
     (nome, cpf, rg, endereco, idade, data_de_nascimento)
@@ -64,9 +68,9 @@ VALUES
 INSERT INTO `funcionarios`
     (nome, cpf, rg, endereco, comissao, cargo, idade, data_de_nascimento, trabalhando_desde)
 VALUES
-    ("Naka", "46302106950", "126817899", "Rua Pão Doce de Barros", 10, "CEO", 20, "2003-01-27", "1945-12-19"),
-    ("Murilo", "46302106950", "126817899", "Rua Pão Doce de Barros", 10, "CEO", 20, "2003-01-27", "1945-12-19"),
-    ("João", "46302106950", "126817899", "Rua Pão Doce de Barros", 10, "CEO", 20, "2003-01-27", "1945-12-19"),
+    ("Naka", "46302106950", "126817899", "Rua Pão Doce de Barros", 99, "CEO", 100, "2005-12-19", "1945-12-19"),
+    ("Matheus", "57413217061", "237928900", "Rua Barros", 50, "Gerente", 60, "2004-02-27", "1945-02-27"),
+    ("Jonathan", "57413217061", "348039011", "Rua Pão", 10, "Vendedor", 21, "2004-01-20", "1945-01-20")
 
 
 INSERT INTO carros
@@ -76,3 +80,36 @@ VALUES
     ("Ford", "Mustang", 4000.00, "2022-05-10"),
     ("Honda", "Civic", 1250.00, "2022-04-09")
 
+INSERT INTO `vendas`
+    (nome_do_funcionario, comissao, valor_da_venda, nome_do_carro, modelo_do_carro, nome_do_cliente, cpf_do_cliente, rg_do_cliente)
+VALUES
+    (
+        (SELECT nome FROM `funcionarios` WHERE cargo = "CEO"),
+        (SELECT comissao FROM `funcionarios` WHERE cargo = "CEO"), 
+        (SELECT valor FROM `carros` WHERE nome = "Ford"),
+        "Ford", 
+        (SELECT modelo FROM `carros` WHERE nome = "Ford"),
+        (SELECT nome FROM `clientes` WHERE rg = "107812010"),
+        (SELECT cpf FROM `clientes` WHERE rg = "107812010"),
+        "107812010"
+    ),
+    (
+        (SELECT nome FROM `funcionarios` WHERE cargo = "Gerente"),
+        (SELECT comissao FROM `funcionarios` WHERE cargo = "Gerente"), 
+        (SELECT valor FROM `carros` WHERE nome = "Toyota"),
+        "Toyota", 
+        (SELECT modelo FROM `carros` WHERE nome = "Toyota"),
+        (SELECT nome FROM `clientes` WHERE rg = "233140153"),
+        (SELECT cpf FROM `clientes` WHERE rg = "233140153"),
+        "233140153"
+    ),
+    (
+        (SELECT nome FROM `funcionarios` WHERE cargo = "Vendedor"),
+        (SELECT comissao FROM `funcionarios` WHERE cargo = "Vendedor"), 
+        (SELECT valor FROM `carros` WHERE nome = "Honda"),
+        "Honda", 
+        (SELECT modelo FROM `carros` WHERE nome = "Honda"),
+        (SELECT nome FROM `clientes` WHERE rg = "422019999"),
+        (SELECT cpf FROM `clientes` WHERE rg = "422019999"),
+        "422019999"
+    )
